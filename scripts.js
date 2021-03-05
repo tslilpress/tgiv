3290167ecba1c25b5674ec4b0585d0a024026b2c
tslilpress <tslilpress@gmail.com>
2021-03-05 16:12:54 -0500

    added fade-in on scroll to each section

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-
+// NAV PULL UP ON HYPERLINK CLICK
 const menuLinks = document.querySelectorAll('.navigation__link')
 
 const uncheck = () => {
@@ -10,5 +10,27 @@ for(i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener('click', uncheck)
 }
 
+
+// FADE IN SECTION ON SCROLL 
+
+const fades = document.querySelectorAll('.fade-in')
+
+const fadeInOnScroll = new IntersectionObserver(function(
+    entries,
+    fadeInOnScroll
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add('appear')
+            fadeInOnScroll.unobserve(entry.target)
+        }
+    })
+})
+
+fades.forEach(fade => {
+    fadeInOnScroll.observe(fade)
+})
 
 
